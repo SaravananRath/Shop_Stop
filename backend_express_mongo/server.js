5cf0f2c49a9d385af0a3f2c4a16725836684b1a6
SaravananRath <saravananr95m16@gmail.com>
2018-07-22 19:22:04 +0530

    Setup and connect to database

--- backend_express_mongo/server.js
+++ backend_express_mongo/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const dbConfig = require('./config/database.config')
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 const app = express()
 const port = 3001
+
+mongoose.connect(dbConfig.url,{ useNewUrlParser: true })
+    .then(()=>{
+        console.log('Succesfully Connected to Database')
+    })
+    .catch(err=>{
+        console.log('Could not connect to the database')
+        process.exit()
+    })
+
+
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(bodyParser.json())
